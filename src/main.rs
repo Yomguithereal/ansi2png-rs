6db55452c255a4336b361b7da871349880baeb2b
Yomguithereal <guillaumeplique@gmail.com>
2026-06-01 15:24:26 +0200

    Hiding italic & bold under text-style feature

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,8 @@ use crate::{
     printer::Settings,
 };
 
-fn main() {
-    let opt = Opt::parse();
-
-    let input: Box<dyn Read> = match &opt.input_path {
-        Some(p) if !matches!(p.to_str(), Some(s) if s == "=") => {
-            Box::new(std::fs::File::open(p).unwrap())
-        }
-        _ => Box::new(std::io::stdin()),
-    };
-
-    let mut input = std::io::BufReader::new(input);
-
+#[cfg(feature = "font-style")]
+fn load_fonts() -> (Font<'static>, Font<'static>, Font<'static>, Font<'static>) {
     flate!(static FONT: [u8] from
         "fonts/iosevka-term-extended.ttf");
 
@@ -44,10 +34,37 @@ fn main() {
     flate!(static FONT_ITALIC_BOLD: [u8] from
         "fonts/iosevka-term-extendedbolditalic.ttf");
 
+    (
+        Font::try_from_bytes(&FONT).unwrap(),
+        Font::try_from_bytes(&FONT_BOLD).unwrap(),
+        Font::try_from_bytes(&FONT_ITALIC).unwrap(),
+        Font::try_from_bytes(&FONT_ITALIC_BOLD).unwrap(),
+    )
+}
+
+#[cfg(not(feature = "font-style"))]
+fn load_fonts() -> (Font<'static>, Font<'static>, Font<'static>, Font<'static>) {
+    flate!(static FONT: [u8] from
+        "fonts/iosevka-term-extended.ttf");
+
     let font = Font::try_from_bytes(&FONT).unwrap();
-    let font_bold = Font::try_from_bytes(&FONT_BOLD).unwrap();
-    let font_italic = Font::try_from_bytes(&FONT_ITALIC).unwrap();
-    let font_italic_bold = Font::try_from_bytes(&FONT_ITALIC_BOLD).unwrap();
+
+    (font.clone(), font.clone(), font.clone(), font)
+}
+
+fn main() {
+    let opt = Opt::parse();
+
+    let input: Box<dyn Read> = match &opt.input_path {
+        Some(p) if !matches!(p.to_str(), Some(s) if s == "=") => {
+            Box::new(std::fs::File::open(p).unwrap())
+        }
+        _ => Box::new(std::io::stdin()),
+    };
+
+    let mut input = std::io::BufReader::new(input);
+
+    let (font, font_bold, font_italic, font_italic_bold) = load_fonts();
 
     let font_height = 50.0;
     let scale = Scale {
